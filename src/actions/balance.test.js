fb523198b7bb976efa486932a2e6ba0f556e4708
Manuel Micu <manu4216@gmail.com>
2018-05-31 13:22:31 +0200

    Add balance reducer and its test

--- src/actions/balance.test.js
+++ src/actions/balance.test.js
@@ -1,7 +1,7 @@
 import * as constants from './constants';
 import * as actions from './balance';
 
-it('creates an actions to set the balance', () => {
+it('creates an action to set the balance', () => {
     const balance = 0;
 
     const expectedAction = { type: constants.SET_BALANCE, balance };
---
+++ src/reducers/balance.js
@@ -0,0 +1,14 @@
+import * as constants from '../actions/constants';
+
+const balance = (state = 0, action) => {
+    switch (action.type) {
+        case constants.SET_BALANCE:
+            return action.balance;
+            break;
+
+        default:
+            return state;
+    }
+};
+
+export default balance;
