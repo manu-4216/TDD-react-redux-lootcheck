2ce5f48b2ff0002d87e4e1e1062743cad5754d29
Manuel Micu <manu4216@gmail.com>
2018-06-01 08:52:56 +0200

    Add input field for deposit or withdraw

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -3,12 +3,27 @@ import { connect } from 'react-redux';
 
 // We need to export this un-connected component for the test
 export class Wallet extends Component {
+    constructor() {
+        super();
+
+        this.state = { balance: undefined };
+    }
+
+    updateBalance = event => {
+        this.setState({ balance: parseInt(event.target.value, 10) });
+    };
+
     render() {
         return (
             <div>
                 <h3 className="balance">
                     Wallet balance: {this.props.balance}
                 </h3>
+                <br />
+                <input
+                    className="input-wallet"
+                    onChange={this.updateBalance}
+                />>
             </div>
         );
     }
